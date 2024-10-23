f6bf47c85f0ce2375fc7edbe73ec1fd4f1ae183a
Dillon Luther <dillon.luther@gmail.com>
2024-10-23 10:20:19 -0400

    refactor with update/3

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -40,6 +40,6 @@ defmodule SlaxWeb.ChatRoomLive do
   end
 
   def handle_event("toggle-topic", _unsigned_params, socket) do
-    {:noreply, assign(socket, :hide_topic?, !socket.assigns.hide_topic?)}
+    {:noreply, update(socket, :hide_topic?, &(!&1))}
   end
 end
