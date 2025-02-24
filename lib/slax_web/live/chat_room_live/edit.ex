182d94f1f4d2cd5d89a6d03911c32e8114e862aa
Dillon Luther <dillon.luther@gmail.com>
2025-02-23 16:47:20 -0800

    extract RoomComponents

--- lib/slax_web/live/chat_room_live/edit.ex
+++ lib/slax_web/live/chat_room_live/edit.ex
@@ -3,6 +3,8 @@ defmodule SlaxWeb.ChatRoomLive.Edit do
 
   alias Slax.Chat
 
+  import SlaxWeb.RoomComponents
+
   def render(assigns) do
     ~H"""
     <div class="mx-auto w-96 mt-12">
@@ -18,13 +20,7 @@ defmodule SlaxWeb.ChatRoomLive.Edit do
         </:actions>
       </.header>
 
-      <.simple_form for={@form} id="room-form" phx-change="validate-room" phx-submit="save-room">
-        <.input field={@form[:name]} type="text" label="Name" phx-debounce />
-        <.input field={@form[:topic]} type="text" label="Topic" phx-debounce />
-        <:actions>
-          <.button phx-disable-with="Saving..." class="w-full">Save</.button>
-        </:actions>
-      </.simple_form>
+      <.room_form form={@form} />
     </div>
     """
   end
@@ -61,10 +57,10 @@ defmodule SlaxWeb.ChatRoomLive.Edit do
     {:noreply, assign_form(socket, changeset)}
   end
 
-  def handle_event("save_room", %{"room" => room_params}, socket) do
+  def handle_event("save-room", %{"room" => room_params}, socket) do
     case Chat.update_room(socket.assigns.room, room_params) do
       {:ok, room} ->
-        {:noreploy,
+        {:noreply,
          socket
          |> put_flash(:info, "Room updated successfully")
          |> push_navigate(to: ~p"/rooms/#{room}")}
